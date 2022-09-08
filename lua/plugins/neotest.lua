56fd006af7fe89719fc5f0577f245d41db65059d
Daniel Pieper <daniel@daniel-pieper.com>
2022-09-08 08:13:32 +0200

    Add neotest summary window binding

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -24,9 +24,9 @@ require("neotest").setup({
 
 vim.keymap.set('n', '<Leader>tt', function () return require("neotest").run.run() end, { silent = true })
 vim.keymap.set('n', '<Leader>tf', function () return require("neotest").run.run(vim.fn.expand("%")) end, { silent = true})
-vim.keymap.set('n', '<Leader>ts', function () return require("neotest").run.stop() end, { silent = true})
 vim.keymap.set('n', '<Leader>ta', function () return require("neotest").run.attach() end, { silent = true})
 vim.keymap.set('n', '<Leader>td', function () return require("neotest").run.run({ strategy = "dap"}) end, { silent = true})
+vim.keymap.set('n', '<Leader>ts', function () return require("neotest").summary.toggle() end, { silent = true})
 
 if utils.isModuleAvailable("which-key") then
   require("which-key").register({
@@ -34,9 +34,9 @@ if utils.isModuleAvailable("which-key") then
       name = "run tests",
       t = "Run nearest test",
       f = "Run tests in file",
-      s = "Stop the nearest test",
       a = "Attach to the nearest test",
       d = "Debug the nearest test",
+      s = "Toggle summary window",
     },
   }, { prefix = "<leader>" })
 end
