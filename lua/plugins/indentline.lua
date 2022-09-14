fa2e99661692eabb8b6cdde66aed9fe625ad5047
Daniel Pieper <daniel.pieper@personio.de>
2022-09-14 14:13:04 +0200

    Update space separator, disable json/md quotes conceal

--- lua/plugins/indentline.lua
+++ lua/plugins/indentline.lua
@@ -1,7 +1,17 @@
 vim.g.indentLine_setColors = 0
 -- vim.g.indentLine_bgcolor_gui = '#151718'
-vim.g.indentLine_char = '▏'
+vim.g.indentLine_char = '┊'
 vim.o.list = true
 vim.o.listchars = "tab:▏ "
 vim.g.indentLine_fileTypeExclude = { 'dashboard' }
 vim.g.indentLine_bufTypeExclude = { 'help', 'terminal' }
+
+local jsonConcealGroup = vim.api.nvim_create_augroup("JsonConcealGroup", { clear = true })
+vim.api.nvim_create_autocmd("InsertEnter jsonc,json", {
+  command = "silent! :IndentLinesDisable",
+  group = jsonConcealGroup,
+})
+vim.api.nvim_create_autocmd("InsertLeave jsonc,json", {
+  command = "silent! :IndentLinesEnable",
+  group = jsonConcealGroup,
+})
