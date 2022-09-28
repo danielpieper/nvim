c18f403287ce5cc8043565cf1bf8c726b4260dc0
Daniel Pieper <daniel.pieper@personio.de>
2022-09-28 10:12:16 +0200

    Update golangci_lint mac settings, add format on save

--- lua/plugins/null-ls-nvim.lua
+++ lua/plugins/null-ls-nvim.lua
@@ -1,37 +1,63 @@
 local null_ls = require("null-ls")
+local is_mac = require("utils").is_mac
 
--- TODO: https://github.com/jose-elias-alvarez/null-ls.nvim/wiki/Formatting-on-save
-
-null_ls.setup({
-  -- debug = true,
-  diagnostics_format = "#{m} (#{s})",
-  sources = {
-    null_ls.builtins.formatting.stylua, -- A fast and opinionated Lua formatter written in Rust.
-    null_ls.builtins.formatting.gofumpt, -- Enforce a stricter format than gofmt, while being backwards compatible.
-    null_ls.builtins.formatting.goimports, -- Updates your Go import lines, adding missing ones and removing unreferenced ones.
+local sources = {
+    -- null_ls.builtins.formatting.stylua, -- A fast and opinionated Lua formatter written in Rust.
+    -- null_ls.builtins.formatting.gofumpt, -- Enforce a stricter format than gofmt, while being backwards compatible.
     null_ls.builtins.diagnostics.eslint, -- Fixes problems in your JavaScript code.
 
     null_ls.builtins.completion.spell.with({
-      filetypes = { "gitcommit", "markdown" },
+        filetypes = { "gitcommit", "markdown" },
     }), -- Spell suggestions completion source.
-
 
     null_ls.builtins.diagnostics.phpcs.with({
         condition = function(utils)
-          return utils.root_has_file({ "phpcs.xml.dist", "phpcs.xml" })
+            return utils.root_has_file({ "phpcs.xml.dist", "phpcs.xml" })
         end,
     }), -- PHP_CodeSniffer is a script that tokenizes PHP, JavaScript and CSS files to detect violations of a defined coding standard.
 
     null_ls.builtins.diagnostics.phpstan.with({
-      extra_args = { "--memory-limit", "4G" },
-      timeout = 60000,
-      method = null_ls.methods.DIAGNOSTICS_ON_SAVE,
+        extra_args = { "--memory-limit", "4G" },
+        timeout = 60000,
+        method = null_ls.methods.DIAGNOSTICS_ON_SAVE,
     }), -- PHP static analysis tool.
+}
 
-    null_ls.builtins.diagnostics.golangci_lint.with({
-        args = { "run", "--fix=false", "--enable-all", "--disable=exhaustivestruct", "--out-format=json", "$DIRNAME", "--path-prefix", "$ROOT" }
-    }), -- A Go linter aggregator.
+if is_mac then
+    table.insert(
+        sources,
+        null_ls.builtins.diagnostics.golangci_lint.with({
+            args = {
+                "run",
+                "--disable-all",
+                "--enable=govet,deadcode,gofmt,goimports,ineffassign,misspell,unconvert,revive,rowserrcheck,sqlclosecheck,bodyclose,staticcheck,exhaustive,gomodguard,depguard,exportloopref,errname,lll",
+                "--deadline 5m",
+                "--out-format json",
+                "$DIRNAME",
+                "--path-prefix",
+                "$ROOT",
+            },
+        })
+    ) -- A Go linter aggregator.
+else
+    table.insert(
+        sources,
+        null_ls.builtins.diagnostics.golangci_lint.with({
+            args = {
+                "run",
+                "--enable-all",
+                "--disable exhaustivestruct",
+                "--out-format json",
+                "$DIRNAME",
+                "--path-prefix",
+                "$ROOT",
+            },
+        })
+    ) -- A Go linter aggregator.
+end
 
-    null_ls.builtins.diagnostics.ktlint, -- An anti-bikeshedding Kotlin linter with built-in formatter.
-  },
+null_ls.setup({
+    -- debug = true,
+    diagnostics_format = "#{m} (#{s})",
+    sources = sources,
 })
