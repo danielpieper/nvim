bea9850e4eb92c8c362cc713822e304b17144528
Daniel Pieper <daniel@daniel-pieper.com>
2022-10-11 19:31:11 +0200

    Update theme

--- lua/plugins/base16-arc.lua
+++ lua/plugins/base16-arc.lua
@@ -2,12 +2,12 @@ local highlights = {
     LineNr = { bg = "none" },
     CursorLineNr = { bg = "none" },
 
-    SignColumn     = { bg = "none" },
-    VertSplit      = { fg = "#151718", bg = "none" },
-    GitSignsAdd    = { fg = "#87af87", bg = "none" },
+    SignColumn = { bg = "none", fg = "#2c323c" },
+    VertSplit = { fg = "#151718", bg = "none" },
+    GitSignsAdd = { fg = "#87af87", bg = "none" },
     GitSignsChange = { fg = "#d19a66", bg = "none" },
     GitSignsDelete = { fg = "#e06c75", bg = "none" },
-    EndOfBuffer    = { fg = "#2c323c" },
+    EndOfBuffer = { fg = "#2c323c" },
 
     -- TODO:
     DiffAdd = { bg = "#262626", fg = "#87af87", reverse = true },
@@ -15,8 +15,8 @@ local highlights = {
     DiffDelete = { bg = "#262626", fg = "#e06c75", reverse = true },
     DiffText = { bg = "#262626", fg = "#61afef", reverse = true },
 
-    TelescopeBorder        = { fg = "#5c6370" },
-    TelescopePromptBorder  = { fg = "#5c6370" },
+    TelescopeBorder = { fg = "#5c6370" },
+    TelescopePromptBorder = { fg = "#5c6370" },
     TelescopeResultsBorder = { fg = "#5c6370" },
     TelescopePreviewBorder = { fg = "#5c6370" },
 
@@ -24,32 +24,32 @@ local highlights = {
     -- Constant = { fg = "#d19a66" },
 
     -- highlights for indent lines:
-    Conceal              = { fg = "#2c323c" },
-    Whitespace           = { fg = "#2c323c" },
+    Conceal = { fg = "#2c323c" },
+    Whitespace = { fg = "#2c323c" },
     NvimTreeIndentMarker = { fg = "#2c323c" },
 
-    Search    = { fg = "#1D1F21", bg = "#d19a66" },
+    Search = { fg = "#1D1F21", bg = "#d19a66" },
     IncSearch = { fg = "#1D1F21", bg = "#e5c07b" },
 
     NvimTreeFolderIcon = { fg = "#61afef" },
     NvimTreeFolderName = { fg = "#61afef" },
 
-    LspDiagnosticsDefaultError       = { fg = "Red", bg = "none" },
-    LspDiagnosticsDefaultWarning     = { fg = "Yellow", bg = "none" },
+    LspDiagnosticsDefaultError = { fg = "Red", bg = "none" },
+    LspDiagnosticsDefaultWarning = { fg = "Yellow", bg = "none" },
     LspDiagnosticsDefaultInformation = { fg = "#61afef", bg = "none" },
-    LspDiagnosticsDefaultHint        = { fg = "LightGray", bg = "none" },
+    LspDiagnosticsDefaultHint = { fg = "LightGray", bg = "none" },
 
-    LspDiagnosticsUnderlineError       = { undercurl = true, fg = "#e06c75", bg = "none" },
-    LspDiagnosticsUnderlineWarning     = { undercurl = true, fg = "#e5c07b", bg = "none" },
+    LspDiagnosticsUnderlineError = { undercurl = true, fg = "#e06c75", bg = "none" },
+    LspDiagnosticsUnderlineWarning = { undercurl = true, fg = "#e5c07b", bg = "none" },
     LspDiagnosticsUnderlineInformation = { undercurl = true, fg = "#61afef", bg = "none" },
-    LspDiagnosticsUnderlineHint        = { undercurl = true, fg = "LightGray", bg = "none" },
+    LspDiagnosticsUnderlineHint = { undercurl = true, fg = "LightGray", bg = "none" },
 
-    BufferCurrent      = { fg = "#61afef", bg = "#1D1F21" },
-    BufferVisible      = { bg = "#1D1F21" },
-    BufferVisibleIcon  = { bg = "#1D1F21" },
-    BufferVisibleMod   = { bg = "#1D1F21" },
+    BufferCurrent = { fg = "#61afef", bg = "#1D1F21" },
+    BufferVisible = { bg = "#1D1F21" },
+    BufferVisibleIcon = { bg = "#1D1F21" },
+    BufferVisibleMod = { bg = "#1D1F21" },
     BufferVisibleIndex = { bg = "#1D1F21" },
-    BufferVisibleSign  = { bg = "#1D1F21" },
+    BufferVisibleSign = { bg = "#1D1F21" },
     BufferInactiveSign = { fg = "Black", bg = "#151718" },
 
     rainbowcol1 = { fg = "#eacd95" },
