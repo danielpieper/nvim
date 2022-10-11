6b9cf49d6018a3a58a0a0b33f509d9ca2a0881ac
Daniel Pieper <daniel@daniel-pieper.com>
2022-10-11 12:50:14 +0200

    Update nvim-cmp completion menu design

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -12,13 +12,12 @@ cmp.setup({
             luasnip.lsp_expand(args.body)
         end
     },
-    -- mapping = cmp.mapping.preset.insert({
     mapping = {
         -- ['<C-n>'] = cmp.mapping.scroll_docs(-4),
         -- ['<C-p>'] = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete(),
-        -- ['<C-e>'] = cmp.mapping.close(),
-        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        -- ['<C-e>'] = cmp.mapping.abort(),
+        ['<CR>'] = cmp.mapping.confirm({ select = false }),
         ["<Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
                 cmp.select_next_item()
@@ -41,23 +40,34 @@ cmp.setup({
             end
         end, { "i", "s" }),
     },
+    window = {
+        completion = {
+            winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None",
+            col_offset = -3,
+            side_padding = 0,
+        },
+    },
     formatting = {
+        fields = { "kind", "abbr", "menu" },
         format = function(entry, vim_item)
-            -- fancy icons and a name of kind
-            vim_item.kind = require("lspkind").presets.default[vim_item.kind] .. " " .. vim_item.kind
+            local kind = require("lspkind").cmp_format({ mode = "symbol_text", maxwidth = 50 })(entry, vim_item)
+            local strings = vim.split(kind.kind, "%s", { trimempty = true })
+            kind.kind = " " .. strings[1] .. " "
+            if entry.source.name == "nvim_lsp" then
+                kind.menu = "    (" .. strings[2] .. ")"
+            else
+                kind.menu = ({
+                    luasnip = "    [Snippet]",
+                    buffer = "    [Buffer]",
+                    path = "    [Path]",
+                    calc = "    [Calc]",
+                    nvim_lua = "    [Lua]",
+                    emoji = "    [Emoji]",
+                    ['vim-dadbod-completion'] = "    [DB]",
+                })[entry.source.name]
+            end
 
-            -- set a name for each source
-            vim_item.menu = ({
-                nvim_lsp = "[LSP]",
-                luasnip = "[Snippet]",
-                buffer = "[Buffer]",
-                path = "[Path]",
-                calc = "[Calc]",
-                nvim_lua = "[Lua]",
-                emoji = "[Emoji]",
-                ['vim-dadbod-completion'] = "[DB]",
-            })[entry.source.name]
-            return vim_item
+            return kind
         end,
     },
     sources = cmp.config.sources({
@@ -90,4 +100,55 @@ cmp.setup.cmdline(':', {
     }, {
         { name = 'cmdline' }
     })
+})
+
+local highlights = {
+    PmenuSel = { bg = "#282C34", fg = "NONE" },
+    Pmenu = { fg = "#C5CDD9", bg = "#1D1F21" },
+
+    CmpItemAbbrDeprecated = { fg = "#7E8294", bg = "NONE", strikethrough = true },
+    CmpItemAbbrMatch = { fg = "#61afef", bg = "NONE", bold = true },
+    CmpItemAbbrMatchFuzzy = { fg = "#61afef", bg = "NONE", bold = true },
+    CmpItemMenu = { fg = "#C792EA", bg = "NONE", italic = true },
+
+    CmpItemKindField = { fg = "#EED8DA", bg = "#B5585F" },
+    CmpItemKindProperty = { fg = "#EED8DA", bg = "#B5585F" },
+    CmpItemKindEvent = { fg = "#EED8DA", bg = "#B5585F" },
+
+    CmpItemKindText = { fg = "#C3E88D", bg = "#9FBD73" },
+    CmpItemKindEnum = { fg = "#C3E88D", bg = "#9FBD73" },
+    CmpItemKindKeyword = { fg = "#C3E88D", bg = "#9FBD73" },
+
+    CmpItemKindConstant = { fg = "#FFE082", bg = "#D4BB6C" },
+    CmpItemKindConstructor = { fg = "#FFE082", bg = "#D4BB6C" },
+    CmpItemKindReference = { fg = "#FFE082", bg = "#D4BB6C" },
+
+    CmpItemKindFunction = { fg = "#EADFF0", bg = "#A377BF" },
+    CmpItemKindStruct = { fg = "#EADFF0", bg = "#A377BF" },
+    CmpItemKindClass = { fg = "#EADFF0", bg = "#A377BF" },
+    CmpItemKindModule = { fg = "#EADFF0", bg = "#A377BF" },
+    CmpItemKindOperator = { fg = "#EADFF0", bg = "#A377BF" },
+
+    CmpItemKindVariable = { fg = "#C5CDD9", bg = "#7E8294" },
+    CmpItemKindFile = { fg = "#C5CDD9", bg = "#7E8294" },
+
+    CmpItemKindUnit = { fg = "#F5EBD9", bg = "#D4A959" },
+    CmpItemKindSnippet = { fg = "#F5EBD9", bg = "#D4A959" },
+    CmpItemKindFolder = { fg = "#F5EBD9", bg = "#D4A959" },
+
+    CmpItemKindMethod = { fg = "#DDE5F5", bg = "#6C8ED4" },
+    CmpItemKindValue = { fg = "#DDE5F5", bg = "#6C8ED4" },
+    CmpItemKindEnumMember = { fg = "#DDE5F5", bg = "#6C8ED4" },
+
+    CmpItemKindInterface = { fg = "#D8EEEB", bg = "#58B5A8" },
+    CmpItemKindColor = { fg = "#D8EEEB", bg = "#58B5A8" },
+    CmpItemKindTypeParameter = { fg = "#D8EEEB", bg = "#58B5A8" },
+}
+
+vim.api.nvim_create_autocmd("Colorscheme", {
+    callback = function()
+        for name, hi in pairs(highlights) do
+            vim.api.nvim_set_hl(0, name, hi)
+        end
+    end,
 })
