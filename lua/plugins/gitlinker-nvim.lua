1290f64ffab209eaf026f6262d11f132ed083416
Daniel Pieper <daniel@daniel-pieper.com>
2023-01-05 12:48:42 +0100

    Add gitlinker copy to clipboard mapping

--- lua/plugins/gitlinker-nvim.lua
+++ lua/plugins/gitlinker-nvim.lua
@@ -5,14 +5,25 @@ gitlinker.setup({
     mappings = nil,
 })
 
-vim.keymap.set('n', '<leader>GG', '<cmd>lua require"gitlinker".get_buf_range_url("n", {action_callback = require"gitlinker.actions".open_in_browser})<cr>', { silent = true })
-vim.keymap.set('v', '<leader>GG', '<cmd>lua require"gitlinker".get_buf_range_url("v", {action_callback = require"gitlinker.actions".open_in_browser})<cr>', {})
+vim.keymap.set('n', '<leader>GG',
+    '<cmd>lua require"gitlinker".get_buf_range_url("n", {action_callback = require"gitlinker.actions".open_in_browser})<cr>'
+    , { silent = true })
+vim.keymap.set('v', '<leader>GG',
+    '<cmd>lua require"gitlinker".get_buf_range_url("v", {action_callback = require"gitlinker.actions".open_in_browser})<cr>'
+    , {})
+vim.keymap.set('n', '<leader>GC',
+    '<cmd>lua require"gitlinker".get_buf_range_url("n", {action_callback = require"gitlinker.actions".copy_to_clipboard})<cr>'
+    , { silent = true })
+vim.keymap.set('v', '<leader>GC',
+    '<cmd>lua require"gitlinker".get_buf_range_url("v", {action_callback = require"gitlinker.actions".copy_to_clipboard})<cr>'
+    , {})
 
 if utils.isModuleAvailable("which-key") then
     require("which-key").register({
         G = {
             name = "Git",
             G = "Open git repo url",
+            C = "Copy git repo url to clipboard",
         },
     }, { prefix = "<leader>" })
 end
--- lua/plugins/nvim-osc52.lua
+++ lua/plugins/nvim-osc52.lua
@@ -1,6 +1,6 @@
 require('osc52').setup {
     max_length = 0, -- Maximum length of selection (0 for no limit)
-    silent = false, -- Disable message on successful copy
+    silent = true, -- Disable message on successful copy
     trim = false, -- Trim text before copy
 }
 
