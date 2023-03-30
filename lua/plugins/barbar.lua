5478a4ae41d3316ef81e8993303c3355e3226759
Daniel Pieper <daniel@daniel-pieper.com>
2023-03-30 12:18:19 +0200

    Update barbar nvim config

--- lua/plugins/barbar.lua
+++ lua/plugins/barbar.lua
@@ -11,58 +11,45 @@ return {
             'https://github.com/nvim-tree/nvim-web-devicons' -- A lua fork of vim-devicons. This plugin provides the same icons as well as colors for each icon.
         },
         opts = {
-            -- Enable/disable animations
-            animation = true,
+            icons = {
+                -- Configure the base icons on the bufferline.
+                buffer_index = false,
+                buffer_number = false,
+                button = false;
+                -- Enables / disables diagnostic symbols
+                diagnostics = {
+                    [vim.diagnostic.severity.ERROR] = { enabled = true, icon = 'ﬀ' },
+                    [vim.diagnostic.severity.WARN] = { enabled = false },
+                    [vim.diagnostic.severity.INFO] = { enabled = false },
+                    [vim.diagnostic.severity.HINT] = { enabled = true },
+                },
+                filetype = {
+                    -- Sets the icon's highlight group.
+                    -- If false, will use nvim-web-devicons colors
+                    custom_colors = false,
 
-            -- Enable/disable icons
-            -- if set to 'numbers', will show buffer index in the tabline
-            -- if set to 'both', will show buffer index and icons in the tabline
-            -- icons = 'both',
-            icons = true,
+                    -- Requires `nvim-web-devicons` if `true`
+                    enabled = true,
+                },
+                separator = { left = '▎', right = '' },
 
-            -- If set, the icon color will follow its corresponding buffer
-            -- highlight group. By default, the Buffer*Icon group is linked to the
-            -- Buffer* group (see Highlighting below). Otherwise, it will take its
-            -- default value as defined by devicons.
-            icon_custom_colors = false,
+                -- Configure the icons on the bufferline when modified or pinned.
+                -- Supports all the base icon options.
+                -- modified = { button = '●' },
 
-            -- Configure icons on the bufferline.
-            icon_separator_active = '▎',
-            icon_separator_inactive = '▎',
-            icon_close_tab = '',
-            icon_pinned = '車',
-            icon_close_tab_modified = '●',
-
-            -- Enable/disable close button
-            closable = false,
-
-            -- Enables/disable clickable tabs
-            -- left-click: go to buffer
-            -- middle-click: delete buffer
-            clickable = true,
-
-            -- If set, the letters for each buffer in buffer-pick mode will be
-            -- assigned based on their name. Otherwise or in case all letters are
-            -- already assigned, the behavior is to assign letters in order of
-            -- usability (see order below)
-            semantic_letters = true,
-
-            -- New buffer letters are assigned in this order. This order is
-            -- optimal for the qwerty keyboard layout but might need adjustement
-            -- for other layouts.
-            letters = 'asdfjkl;ghnmxcbziowerutyqpASDFJKLGHNMXCBZIOWERUTYQP',
+                -- Configure the icons on the bufferline based on the visibility of a buffer.
+                -- Supports all the base icon options, plus `modified` and `pinned`.
+                alternate = { filetype = { enabled = false } },
+                -- current = { buffer_index = true },
+                -- inactive = { button = '×' },
+                visible = { modified = { buffer_number = false } },
+            },
 
             -- Sets the maximum padding width with which to surround each tab
             maximum_padding = 0,
 
             -- Sets the maximum buffer name length.
             maximum_length = 40,
-
-            -- Enable/disable auto-hiding the tab bar when there is a single buffer
-            auto_hide = false,
-
-            -- Hide inactive buffers and file extensions. Other options are `alternate`, `current`, and `visible`.
-            -- hide = { extensions = true, inactive = false },
         },
         keys = {
             -- Magic buffer-picking mode
