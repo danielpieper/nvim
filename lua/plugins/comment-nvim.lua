55998678fec3447ba993a7308c6348cdbe32d840
Daniel Pieper <daniel@daniel-pieper.com>
2023-05-25 10:47:21 +0200

    Replace kommentary with comment.nvim

---
+++ lua/plugins/comment-nvim.lua
@@ -0,0 +1,10 @@
+-- *****************************************
+-- Comments
+-- *****************************************
+return {
+    {
+        -- Smart and powerful comment plugin for neovim. Supports treesitter, dot repeat, left-right/up-down motions, hooks, and more
+        'https://github.com/numToStr/Comment.nvim',
+        config = true,
+    },
+}
