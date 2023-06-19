6188b683b1eb055637677c44436ab89667e7b4c0
Daniel Pieper <daniel@daniel-pieper.com>
2023-06-19 13:07:17 +0200

    Use fidget-nvim legacy tag

--- lua/plugins/fidget-nvim.lua
+++ lua/plugins/fidget-nvim.lua
@@ -5,6 +5,7 @@ return {
     {
         -- Standalone UI for nvim-lsp progress
         'https://github.com/j-hui/fidget.nvim',
+        tag = 'legacy',
         config = function()
             require("fidget").setup {}
         end,
