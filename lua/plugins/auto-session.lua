b7ebe3495f5ea9f873f874b89ac7eca628948c43
Daniel Pieper <daniel@daniel-pieper.com>
2023-07-17 08:50:05 +0200

    Update focus settings

--- lua/plugins/auto-session.lua
+++ lua/plugins/auto-session.lua
@@ -9,10 +9,10 @@ return {
         opts = {
             log_level = "error",
             -- auto_session_use_git_branch = true,
-            pre_save_cmds = { "FocusDisable" },
-            post_save_cmds = { "FocusEnable" },
-            pre_restore_cmds = { "FocusDisable" },
-            post_restore_cmds = { "FocusEnable" },
+            -- pre_save_cmds = { "FocusDisable" },
+            -- post_save_cmds = { "FocusEnable" },
+            -- pre_restore_cmds = { "FocusDisable" },
+            -- post_restore_cmds = { "FocusEnable" },
         },
     },
 }
--- lua/plugins/focus-nvim.lua
+++ lua/plugins/focus-nvim.lua
@@ -6,7 +6,7 @@ return {
         -- Auto-Focusing and Auto-Resizing Splits/Windows for Neovim written in Lua! Vim splits on steroids.
         'https://github.com/beauwilliams/focus.nvim',
         name = "focus",
-        opts = {
+        ui = {
             hybridnumber = true,
             cursorline = false,
         },
