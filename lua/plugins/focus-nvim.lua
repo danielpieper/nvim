412e5744912880e8ba01ce585b5eb8bf298925b3
Daniel Pieper <daniel@daniel-pieper.com>
2023-08-23 20:14:18 +0200

    Update focus.nvim repo url

--- lua/plugins/focus-nvim.lua
+++ lua/plugins/focus-nvim.lua
@@ -4,11 +4,15 @@
 return {
     {
         -- Auto-Focusing and Auto-Resizing Splits/Windows for Neovim written in Lua! Vim splits on steroids.
-        'https://github.com/beauwilliams/focus.nvim',
-        name = "focus",
-        ui = {
-            hybridnumber = true,
-            cursorline = false,
+        'https://github.com/nvim-focus/focus.nvim',
+        version = false,
+        main = "focus",
+        opts = {
+            enable = true,
+            ui = {
+                hybridnumber = true,
+                cursorline = false,
+            },
         },
     },
 }
