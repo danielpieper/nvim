f9e5450fd75d16b29ff44d4917bc4dda89510ac0
Daniel Pieper <daniel@daniel-pieper.com>
2023-12-14 17:30:28 +0100

    Configure colorscheme, add languages

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "onedark",
+    },
+  },
+  {
+    "navarasu/onedark.nvim", -- https://github.com/navarasu/onedark.nvim
+    opts = {
+      style = "darker",
+    },
+  },
+}
---
+++ lua/plugins/core.lua
@@ -0,0 +1,6 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    enabled = false, -- won't work on nixos
+  },
+}
