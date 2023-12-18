e63171595d33bc772b5f8f588f9c5fda73447b78
Daniel Pieper <daniel@daniel-pieper.com>
2023-12-18 11:28:49 +0100

    Add copy rel path/file keys

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -23,5 +23,20 @@ local function paste_blank_line_below()
   paste_blank_line(vim.fn.line("."))
 end
 
-vim.keymap.set("n", "[<Space>", paste_blank_line_above)
-vim.keymap.set("n", "]<Space>", paste_blank_line_below)
+vim.keymap.set("n", "[<Space>", paste_blank_line_above, { desc = "Add blank line above" })
+vim.keymap.set("n", "]<Space>", paste_blank_line_below, { desc = "Add blank line below" })
+
+local function copy_rel_path()
+  local path = string.sub(vim.fn.expand("%:h"), string.len(vim.fn.getcwd()) + 2)
+  vim.fn.setreg("+", path)
+  vim.notify('Copied "' .. path .. '" to the clipboard!')
+end
+
+local function copy_rel_file()
+  local path = string.sub(vim.fn.expand("%"), string.len(vim.fn.getcwd()) + 2)
+  vim.fn.setreg("+", path)
+  vim.notify('Copied "' .. path .. '" to the clipboard!')
+end
+
+vim.keymap.set("n", "<Leader>by", copy_rel_path, { desc = "Copy relative path" })
+vim.keymap.set("n", "<Leader>bY", copy_rel_file, { desc = "Copy relative file" })
