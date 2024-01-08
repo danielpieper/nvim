3837d168b3e6f737e4a09a45ec761adf4ec968e4
Daniel Pieper <daniel@daniel-pieper.com>
2024-01-08 11:49:29 +0100

    Add intelephense

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -8,6 +8,22 @@ return {
             ["nil"] = { formatting = { command = { "nixpkgs-fmt" } } },
           },
         },
+        intelephense = {
+          filetypes = { "php", "blade" },
+          init_options = {
+            licenceKey = "${config.xdg.configHome}/intelephense/license.txt",
+            -- clearCache = true
+          },
+          settings = {
+            intelephense = {
+              filetypes = { "php", "blade" },
+              files = {
+                associations = { "*.php", "*.blade.php" }, -- Associating .blade.php files as well
+                maxSize = 5000000,
+              },
+            },
+          },
+        },
       },
     },
   },
